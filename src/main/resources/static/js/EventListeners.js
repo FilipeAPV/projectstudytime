6914b6b9172528030a0152c6227f355a645dd804
FILIPE VIEIRA <no@no.com>
2022-10-31 10:25:47 +0100

    STD-16: js code refactoring (EventListeners.js)

--- src/main/resources/static/js/EventListeners.js
+++ src/main/resources/static/js/EventListeners.js
@@ -1,25 +1,47 @@
+/*Allow Tab inside textAreas*/
+function allowTabInsideTextArea(textAreaField) {
+
+    textAreaField.addEventListener('keydown', (key) => {
+        if (key.code === "Tab") {
+            key.preventDefault();
+            textAreaField.setRangeText(
+                '      ', // 6 empty spaces
+                textAreaField.selectionStart,
+                textAreaField.selectionStart,
+                'end'
+            )
+        }
+    });
+}
+
 window.onload = function () {
     const saveBtn = document.getElementById("saveBtn");
     const sessionForm = document.getElementById("sessionForm");
+    const sessionContent = document.getElementById("sessionContent");
+    const sessionFeelings = document.getElementById("sessionFeelings");
+    const testBtn2 = document.getElementById("testbtn2");
 
-    // Set value (date) of the hidden form field
-    function setDateToHiddenValue() {
-        const date = new Date();
 
-        let month = date.getMonth() + 1;
-        if (month < 10) {month = '0' + month;}
-
-        let day = date.getDate();
-        if (day < 10) {day = '0' + day;}
-
-        const formattedDate = date.getFullYear() + '-' + month + '-' + day;
-        document.getElementById("sessionDate").setAttribute("value", formattedDate);
-    }
+    displayTime();
+    displayDayMonthYear();
+    allowTabInsideTextArea(sessionContent);
+    allowTabInsideTextArea(sessionFeelings);
 
     // Save Session from Modal for Preview
     saveBtn.addEventListener("click", function (){
         setDateToHiddenValue();
         sessionForm.submit();
+    });
+
+    //Textarea validation
+    testBtn2.addEventListener("click", function (){
+        const markdownDiv = document.getElementById("markdownPreview");
+        markdownDiv.innerHTML = "";
+        document.getElementById("textarea-hidden").value = "";
+
+        let isValidated = validateAndIdentTextArea("sessionContent", true);
+        if (isValidated) { isValidated = validateAndIdentTextArea("sessionFeelings", false); }
+        if (isValidated) {showModalPreview();}
     });
 }
 
--- src/main/resources/static/js/MyScripts.js
+++ src/main/resources/static/js/MyScripts.js
@@ -148,25 +148,28 @@ function calcStudySessionTimer() {
 }
 
 /**
+ * Session Form
+ */
+
+// Set value (date) of the hidden form field
+function setDateToHiddenValue() {
+    const date = new Date();
+
+    let month = date.getMonth() + 1;
+    if (month < 10) {month = '0' + month;}
+
+    let day = date.getDate();
+    if (day < 10) {day = '0' + day;}
+
+    const formattedDate = date.getFullYear() + '-' + month + '-' + day;
+    document.getElementById("sessionDate").setAttribute("value", formattedDate);
+}
+
+/**
  * TextArea
  */
 
-/*Allow Tab inside textAreas*/
-function allowTabInsideTextArea(elementId) {
-    const textAreaField = document.getElementById(elementId);
 
-    textAreaField.addEventListener('keydown', (key) => {
-        if (key.code === "Tab") {
-            key.preventDefault();
-            textAreaField.setRangeText(
-                '      ', // 6 empty spaces
-                textAreaField.selectionStart,
-                textAreaField.selectionStart,
-                'end'
-            )
-        }
-    });
-}
 
 // Return an Array of all the lines inside the text area
 function getLines(textArea) {
